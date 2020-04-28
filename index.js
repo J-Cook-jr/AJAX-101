898735dbbd3e1af67cf25aae73e524f3e0990635
Jeff Cook <jecook2692@gmail.com>
2020-04-28 11:41:35 -0400

    adds GET request to js

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ const dogButton = document.getElementById('dog-button');
 
 dogButton.addEventListener('click', function () {
     dogButton.textContent = 'Generating Doggo...';
+    $.get('https://dog.ceo/api/breeds/image/random')
 });
